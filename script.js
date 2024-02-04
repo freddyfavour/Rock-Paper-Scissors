b25a9139fe85d0f49ed3ff56fa3e349e4a4f8fb0
freddyfavour <alfredfavour76@gmail.com>
2024-02-04 16:00:09 +0100

    rps-ui

--- script.js
+++ script.js
@@ -1,48 +1,65 @@
-function getComputerChoice(){
-    let ComputerChoices = ['rock','paper','scissors'];
-    return ComputerChoices[Math.floor(Math.random()*3)];
+let para = document.getElementById("message");
+let player = document.getElementById("player");
+let computer = document.getElementById("computer");
+let main = document.getElementById("main");
+let restart = document.getElementById("restart");
+let announce = document.getElementById("announce");
+var playerScore = 0;
+var computerScore = 0;
+
+function getComputerChoice() {
+  let ComputerChoices = ["rock", "paper", "scissors"];
+  return ComputerChoices[Math.floor(Math.random() * 3)];
 }
 
-function playRound(playerSelection,computerSelection){
+function game(playerSelection) {
+  let computerSelection = getComputerChoice();
+
+  function playRound(playerSelection, computerSelection) {
     if (
-        playerSelection==='rock' && computerSelection==='scissors' ||
-        playerSelection==='paper' && computerSelection==='rock' ||
-        playerSelection==='scissors' && computerSelection==='paper'
-    ){
-        return `${playerSelection} beats ${computerSelection}\n You win`;
+      (playerSelection === "rock" && computerSelection === "scissors") ||
+      (playerSelection === "paper" && computerSelection === "rock") ||
+      (playerSelection === "scissors" && computerSelection === "paper")
+    ) {
+      playerScore++;
+      return `It's always great to trust the 力 within you\n The ${playerSelection} clan beats the clan of ${computerSelection}\n You win`;
+    } else if (
+      (computerSelection === "rock" && playerSelection === "scissors") ||
+      (computerSelection === "paper" && playerSelection === "rock") ||
+      (computerSelection === "scissors" && playerSelection === "paper")
+    ) {
+      computerScore++;
+      return `The clan of ${computerSelection} beats ${playerSelection}\n I guess you haven't mastered the are of listening to your 力\n You lose`;
+    } else {
+      return "Your 力 is at the same level as that of o kakós\nIt's a tie";
     }
-    else if(playerSelection===computerSelection){
-        return "It's a tie,play again";
-    }
-    else{
-        return `${computerSelection} beats ${playerSelection}\n You lose`;
-    }
+  }
+
+  const result = playRound(playerSelection, computerSelection);
+  para.innerText = result;
+  player.innerText = `You : ${playerScore}`;
+  computer.innerText = `o kakós: ${computerScore}`;
+  console.log(playerScore + " player");
+  console.log(computerScore + " comp");
+
+  if (playerScore === 5 || computerScore === 5) {
+    getWinner();
+    endgame();
+  }
 }
 
-function game(){
-    let playerScore = 0;
-    let computerScore = 0;
-
-    while(playerScore<5 || playerScore<5){
-        let computerSelection = getComputerChoice();
-        let playerSelection = prompt('Rock,Paper or Scissors').toLowerCase();
-
-        const result = playRound(playerSelection,computerSelection);
-        if(result.includes('win')){
-        playerScore++;
-        }
-        else if (result.includes('lose')){
-            computerScore++;
-        }
-        console.log(`Current Scores\nPlayer: ${playerScore}\nComputer: ${computerScore}`)
-    }
-
-    if(playerScore>computerScore){
-        console.log('You win');
-    }
-    else{
-        console.log('You lose!');
-    }
+function endgame() {
+  playerScore = 0;
+  computerScore = 0;
+  main.style.filter = "blur(20px)";
+  restart.style.display = "flex";
+  announce.innerText = `${getWinner()}!!!`;
 }
 
-game();
+function getWinner() {
+  if (playerScore > computerScore) {
+    return "You have defeated o kakós";
+  } else {
+    return "o kakós has won and covered the Monde with darkness";
+  }
+}
